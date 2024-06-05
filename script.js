ff20f6adce1ab82b07d782d1f08bd4037bae8973
Areej Omar Abumuhfouz <areejabumahfouz@gmail.com>
2024-06-05 16:07:01 +0300

    added by areej

---
+++ script.js
@@ -0,0 +1,48 @@
+//  Question 1
+//  function reverse(){
+//    const num=window.prompt("enter numbers",""); 
+//    let arr=parseInt(num.split('').reverse().join('')) 
+//    window.alert(arr);
+// }
+// reverse();
+
+//  Question 2
+// for(let i=0;i<=15;i++){
+//     if(i%2==0){
+//         console.log("even")
+//     }
+//     else{
+//         console.log("odd") 
+//     }
+// }
+
+//  Question 3
+//  function reverse(){
+//    const num=window.prompt("Enter words",""); 
+//    let arr=num.split('').sort().join('');
+//    window.alert(arr);
+// }
+// reverse();
+
+
+//  Question 4
+// function dashes(){
+//    const num=window.prompt("Enter numbers:",""); 
+//    let arr=num.split('');
+//    for(let i=0;i<=arr.length;i++){
+//     console.log("kkkkkkkk")
+//     if (arr[i]%2===0 && arr[i+1]%2===0){
+//        arr[i]=arr[i]+"-";
+//        }
+//    }
+//    const result=arr.join('');
+//    window.alert(result);
+// }
+// dashes();
+
+//  Question 5
+// function Agechecker(){
+//     const age=window.prompt("Enter your age!!",""); 
+//     age>=18? alert("The user is Adult") :alert("The user is Minor") ;
+// }
+// Agechecker();
